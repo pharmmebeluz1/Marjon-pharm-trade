5359965e3576413b0b7e334d01cc580e2c0401d7
pharmmebeluz1 <ubaydullayevzuhriddin852@gmail.com>
2026-07-25 13:28:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+from flask import send_from_directory
 from marjon_app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=app.config["APP_ENV"] != "production")
+
+@app.route("/mijoz")
+def mijoz():
+    return send_from_directory(".", "mijoz-faollik.html")
+
+
+if name == "main":
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=app.config.get("APP_ENV") != "production"
+    )
